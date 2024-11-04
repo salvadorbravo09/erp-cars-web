fc3af46df1b9b99e18da6ca2777ddc51e966f8a2
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-04 17:55:44 -0300

    create routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CarFormComponent } from './car-form/car-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Pagina de inicio',
+  },
+  {
+    path: 'car-form/:id',
+    component: CarFormComponent,
+    title: 'Formulario de vehiculos',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
