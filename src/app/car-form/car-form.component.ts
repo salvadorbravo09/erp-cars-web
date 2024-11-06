8b8a6227b7c0767ab80bedd93879d05f8fcae681
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-11-05 22:25:31 -0300

    add implementation for obtain car by id

--- src/app/car-form/car-form.component.ts
+++ src/app/car-form/car-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -6,6 +6,8 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CarService } from '../services/car.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-car-form',
@@ -13,16 +15,44 @@ import {
   imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './car-form.component.html',
 })
-export class CarFormComponent {
+export class CarFormComponent implements OnInit {
   formCar!: FormGroup;
+  isSaveInProgress: boolean = false;
+  edit: boolean = false;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private carService: CarService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {
     this.formCar = this.fb.group({
+      id: [null],
       make: ['', Validators.required],
       model: ['', Validators.required],
       year: ['', Validators.required, Validators.minLength(1886)],
       patent: ['', Validators.required],
       color: ['', Validators.required],
+    });
+  }
+  ngOnInit(): void {
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id !== 'new') {
+      this.edit = true;
+    }
+  }
+
+  getCarById(id: number) {
+    this.carService.getCarById(id).subscribe({
+      next: (foundCar) => {
+        this.formCar.patchValue(foundCar);
+      },
+      error: () => {
+        console.error('Error al obtener el auto');
+      },
+      complete: () => {
+        this.router.navigateByUrl('/');
+      },
     });
   }
 }
